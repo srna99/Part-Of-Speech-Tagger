2f9bdd7cb33c2ad63a27417f6a3aad8b8a05e387
Serena Cheng <serena.ch1999@gmail.com>
2020-03-07 13:47:56 -0500

    Add scorer.py.

---
+++ scorer.py
@@ -0,0 +1,14 @@
+"""
+Serena Cheng
+CMSC 416 - PA 3: POS Tagger (scorer.py)
+3/8/2020
+~~~~~
+Problem:
+
+Usage:
+
+Ex.
+
+Algorithm:
+
+"""
---
+++ tagger.py
@@ -0,0 +1,32 @@
+"""
+Serena Cheng
+CMSC 416 - PA 3: POS Tagger (tagger.py)
+3/8/2020
+~~~~~
+Problem:
+
+Usage:
+
+Ex. 
+    
+Algorithm:
+
+"""
+"""
+For training: keep track of how often see word with tag and tags after another tag
+(Dict[pos[i]][word[i]] or dict[pos[i]][pos[i+1]])
+2 bigram tables (word/tag and tag/tag) and 1 unigram table (total tags)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
