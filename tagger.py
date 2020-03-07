5bf1462d8a6add6315c99155b0bf6d5bfa84e880
Serena Cheng <serena.ch1999@gmail.com>
2020-03-07 17:07:16 -0500

    Parse training set.

--- tagger.py
+++ tagger.py
@@ -18,10 +18,36 @@ For training: keep track of how often see word with tag and tags after another t
 2 bigram tables (word/tag and tag/tag) and 1 unigram table (total tags)
 """
 
+import sys
 
+train_pos = sys.argv[1]
+test_pos = sys.argv[2]
 
+word_pos_dict = {}
+pos_pos_dict = {}
+pos_dict = {}
 
+train_content = []
 
+with open(train_pos, 'r', encoding="utf-8-sig") as file:
+    train_content.extend(file.read().split())
+
+for term in train_content:
+    if term == '[' or term == ']':
+        continue
+    else:
+        parts = term.rsplit('/', 1)
+
+        word = parts[0]
+        pos = parts[1]
+
+        if word in word_pos_dict.keys():
+            if pos in word_pos_dict[word]:
+                word_pos_dict[word][pos] += 1
+            else:
+                word_pos_dict[word][pos] = 1
+        else:
+            word_pos_dict[word] = {pos: 1}
 
 
 
