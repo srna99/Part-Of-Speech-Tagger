40d30a3fea96c604956a40314f5b583d42bafce8
Serena Cheng <serena.ch1999@gmail.com>
2020-03-08 16:39:52 -0400

    Separate tags and set up confusion matrix.

--- scorer.py
+++ scorer.py
@@ -18,26 +18,54 @@ import sys
 test_with_tags = sys.argv[1]
 test_key = sys.argv[2]
 
-tagged_test_content = []
-key_content = []
+test_tagged_content = []
+test_key_content = []
+
+tags_test = []
+tags_key = []
+
+all_tags = set()
+confusion_matrix = {}
 
 baseline_accuracy = 0
 accuracy = 0
 
+
+def separate_tags(array, sep_array):
+    for term in array:
+        # skip brackets
+        if term == '[' or term == ']':
+            continue
+
+        # separate words from tags
+        parts = term.rsplit('/', 1)
+        pos = parts[1].split('|')[0]
+        sep_array.append(pos)
+
+
 with open(test_with_tags, 'r', encoding="utf-8-sig") as file:
-    tagged_test_content.extend(file.read().split())
+    test_tagged_content.extend(file.read().split())
 
 with open(test_key, 'r', encoding="utf-8-sig") as file:
-    key_content.extend(file.read().split())
+    test_key_content.extend(file.read().split())
 
-correct_count = 0
-for ind, term in enumerate(key_content):
-    if term == tagged_test_content[ind] or term == '[' or term == ']':
-        correct_count += 1
+separate_tags(test_tagged_content, tags_test)
+separate_tags(test_key_content, tags_key)
 
-accuracy = correct_count / len(key_content)
-print(accuracy)
+all_tags.update(set(tags_key))
 
+for tag1 in all_tags:
+    for tag2 in all_tags:
+        if tag1 in confusion_matrix.keys():
+            confusion_matrix[tag1][tag2] = 0
+        else:
+            confusion_matrix[tag1] = {tag2: 0}
 
-
+# correct_count = 0
+# for ind, term in enumerate(test_key_content):
+#     if term == test_tagged_content[ind] or term == '[' or term == ']':
+#         correct_count += 1
+#
+# accuracy = correct_count / len(test_key_content)
+# print(accuracy)
 
