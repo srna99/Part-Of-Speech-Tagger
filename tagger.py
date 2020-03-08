0b166d7fbc27a822a82c257947010c53f779e7dd
Serena Cheng <serena.ch1999@gmail.com>
2020-03-08 13:20:03 -0400

    Tag test data after being trained.

--- tagger.py
+++ tagger.py
@@ -12,65 +12,87 @@ Ex.
 Algorithm:
 
 """
-"""
-For training: keep track of how often see word with tag and tags after another tag
-(Dict[pos[i]][word[i]] or dict[pos[i]][pos[i+1]])
-2 bigram tables (word/tag and tag/tag) and 1 unigram table (total tags)
-"""
 
 import sys
 
 train_pos = sys.argv[1]
 test_pos = sys.argv[2]
 
+train_content = []
+test_content = []
+
 word_pos_dict = {}
 pos_pos_dict = {}
 pos_dict = {}
 
-train_content = []
+test_with_tags = []
 
 with open(train_pos, 'r', encoding="utf-8-sig") as file:
     train_content.extend(file.read().split())
 
-prev_pos = "."
+prev_pos = '.'
 for term in train_content:
     if term == '[' or term == ']':
         continue
+
+    parts = term.rsplit('/', 1)
+
+    word = parts[0]
+    pos = parts[1].split('|')[0]
+
+    if word in word_pos_dict.keys():
+        if pos in word_pos_dict[word]:
+            word_pos_dict[word][pos] += 1
+        else:
+            word_pos_dict[word][pos] = 1
     else:
-        parts = term.rsplit('/', 1)
+        word_pos_dict[word] = {pos: 1}
 
-        word = parts[0]
-        pos = parts[1].split('|')[0]
+    if pos in pos_dict:
+        pos_dict[pos] += 1
+    else:
+        pos_dict[pos] = 1
 
-        if word in word_pos_dict.keys():
-            if pos in word_pos_dict[word]:
-                word_pos_dict[word][pos] += 1
+    if prev_pos in pos_pos_dict.keys():
+        if pos in pos_pos_dict[prev_pos]:
+            pos_pos_dict[prev_pos][pos] += 1
+        else:
+            pos_pos_dict[prev_pos][pos] = 1
+    else:
+        pos_pos_dict[prev_pos] = {pos: 1}
+
+    prev_pos = pos
+
+with open(test_pos, 'r', encoding="utf-8-sig") as file:
+    test_content.extend(file.read().split())
+
+prev_pos = '.'
+for word in test_content:
+    if word == '[' or word == ']':
+        test_with_tags.append(word)
+        continue
+
+    max_tag = "NN"
+    max_prob = 0
+
+    if word in word_pos_dict.keys():
+        for pos in word_pos_dict[word].keys():
+            prob_word_pos = word_pos_dict[word][pos] / pos_dict[pos]
+
+            if pos in pos_pos_dict[prev_pos].keys():
+                prob_pos_pos = pos_pos_dict[prev_pos][pos] / pos_dict[prev_pos]
             else:
-                word_pos_dict[word][pos] = 1
-        else:
-            word_pos_dict[word] = {pos: 1}
+                prob_pos_pos = 0
 
-        if pos in pos_dict:
-            pos_dict[pos] += 1
-        else:
-            pos_dict[pos] = 1
+            probability = round(prob_word_pos * prob_pos_pos, 6)
 
-        if prev_pos in pos_pos_dict.keys():
-            if pos in pos_pos_dict[prev_pos]:
-                pos_pos_dict[prev_pos][pos] += 1
-            else:
-                pos_pos_dict[prev_pos][pos] = 1
-        else:
-            pos_pos_dict[prev_pos] = {pos: 1}
+            if probability > max_prob:
+                max_prob = probability
+                max_tag = pos
 
-        prev_pos = pos
+            prev_pos = pos
 
+    word += ('/' + max_tag)
+    test_with_tags.append(word)
 
-
-
-
-
-
-
-
-
+print(" ".join(test_with_tags))
