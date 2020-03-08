7932f8efdf078e36e7c0f96f0ce5b238e0d94338
Serena Cheng <serena.ch1999@gmail.com>
2020-03-08 14:04:37 -0400

    Calculate rough accuracy of constructed test with tags from tagger.py.

--- scorer.py
+++ scorer.py
@@ -12,3 +12,32 @@ Ex.
 Algorithm:
 
 """
+
+import sys
+
+test_with_tags = sys.argv[1]
+test_key = sys.argv[2]
+
+tagged_test_content = []
+key_content = []
+
+baseline_accuracy = 0
+accuracy = 0
+
+with open(test_with_tags, 'r', encoding="utf-8-sig") as file:
+    tagged_test_content.extend(file.read().split())
+
+with open(test_key, 'r', encoding="utf-8-sig") as file:
+    key_content.extend(file.read().split())
+
+correct_count = 0
+for ind, term in enumerate(key_content):
+    if term == tagged_test_content[ind] or term == '[' or term == ']':
+        correct_count += 1
+
+accuracy = correct_count / len(key_content)
+print(accuracy)
+
+
+
+
